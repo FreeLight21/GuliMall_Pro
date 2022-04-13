969cc786cb1dce80fa6f09b3c43121dbb0c7b56f
LightHigher <1651555193@qq.com>
2022-04-13 15:35:30 +0800

    Initial commit

---
+++ gulimall-product/src/main/java/com/lighthigh/gulimall/prodcut/GulimallProdcutApplication.java
@@ -0,0 +1,13 @@
+package com.lighthigh.gulimall.prodcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProdcutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProdcutApplication.class, args);
+    }
+
+}
---
+++ gulimall-product/src/test/java/com/lighthigh/gulimall/prodcut/GulimallProdcutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lighthigh.gulimall.prodcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProdcutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
